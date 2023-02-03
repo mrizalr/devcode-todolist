9dcf97250e9ac3b2a8941030191b54061af5b99d
rizal <muhammadrizal2252@gmail.com>
2023-02-03 16:59:00 +0700

    add docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,16 @@
+# # mysql
+
+# FROM mysql:8.0.32
+
+# ENV MYSQL_ALLOW_EMPTY_PASSWORD=yes
+
+# COPY ./initdb.sql /tmp
+
+# CMD [ "mysqld", "--init-file=/tmp/initdb.sql" ]
+
+
+# golang
+
 FROM golang:1.19-alpine
 
 WORKDIR /app
---
+++ initdb.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS `activities`;
+CREATE TABLE `activities` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(30) DEFAULT NULL,
+  `email` longtext,
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+DROP TABLE IF EXISTS `todos`;
+CREATE TABLE `todos` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `activity_id` bigint unsigned DEFAULT NULL,
+  `title` varchar(30) DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT NULL,
+  `priority` varchar(30) DEFAULT NULL,
+  `created_at` datetime(3) DEFAULT NULL,
+  `updated_at` datetime(3) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_todos_activity` (`activity_id`),
+  CONSTRAINT `fk_todos_activity` FOREIGN KEY (`activity_id`) REFERENCES `activities` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
